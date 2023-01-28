4a48156e65bc85b19a211024e79bda5e30cb160a
GBenakidis <john.benakidis@hotmail.com>
2023-01-28 13:25:37 +0200

    Create Upcoming TableView inside Upcoming Tab

--- Netflix-Clone-2.0/Controllers/UpcomingVC.swift
+++ Netflix-Clone-2.0/Controllers/UpcomingVC.swift
@@ -1,10 +1,58 @@
 import UIKit
 
 class UpcomingVC: UIViewController {
+    private var titles: [Title] = [Title]() // Empty array at assignment
+    
+    private let upcomingTable: UITableView = {
+        // Unonymus closure
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        title = "Upcoming" // Set title
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(upcomingTable)
+        // Set the delegate for TableView
+        upcomingTable.delegate = self
+        upcomingTable.dataSource = self
+        
+        fetchUpcoming()
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTable.frame = view.bounds
+    }
+    
+    private func fetchUpcoming() {
+        APICaller.shared.getUpcomingMovies { [weak self] result in
+            switch result {
+                case .success(let titles):
+                    self?.titles = titles
+                    DispatchQueue.main.async {
+                        self?.upcomingTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
 
+extension UpcomingVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = titles[indexPath.row].original_name ?? titles[indexPath.row].original_title ?? "Unkown"
+        return cell
+    }
 }
---
+++ Netflix-Clone-2.0/Views/TitleTableViewCell.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+    static let identifier = "TitleTableViewCell"
+    
+    private let titlePosterUIImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+}
